5e9776f9d2b564ab1e3c93238bc2a219d9248e6e
rabah babaci <4rabah@gmail.com>
2020-03-13 17:49:50 -0700

    create the Header component

---
+++ github-user-card/src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import logo from '../images/githublogo.png';
+
+export default function Header() {
+  return (
+    <div>
+      <img src={logo} alt='github logo' />
+    </div>
+  );
+}
