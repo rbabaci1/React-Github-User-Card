26e52a8425f2cfbbe4ae43a5c4127f0958f14af7
rabah babaci <4rabah@gmail.com>
2020-03-13 18:17:40 -0700

    convert App to a class component

--- github-user-card/src/App.js
+++ github-user-card/src/App.js
@@ -1,14 +1,13 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import Header from './components/Header';
 
-function App() {
-  return (
-    <div className='App'>
-      <h1>Test</h1>
-      <Header />
-    </div>
-  );
+export default class App extends Component {
+  render() {
+    return (
+      <div className='App'>
+        <Header />
+      </div>
+    );
+  }
 }
-
-export default App;
